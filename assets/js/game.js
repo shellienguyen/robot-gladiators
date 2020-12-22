4b33a08bd565065c698ecd377431a687bd7b10fa
sidonie <shelliednnguyen@gmail.com>
2020-12-21 17:12:34 -0800

    tested variables and functions

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,18 @@
-window.alert( "This is an alert! JavaScript is running!" );
+var playerName = window.prompt( "What is your robot's name?" );
+
+console.log(playerName);
+console.log( "This logs a string, good for leaving yourself a message" );
+
+// This will do math and log 20
+console.log( 10 + 10 );
+
+console.log( "Our robot's name is: " + playerName );
+
+function fight() {
+    window.alert( "The fight has begun!" );
+}
+
+var name = "Shellie";
+console.log( name );
+
+//fight();
