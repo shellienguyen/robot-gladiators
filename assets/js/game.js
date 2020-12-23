b825bf6848e677cb73c2326b6e6b65d273a6e071
sidonie <shelliednnguyen@gmail.com>
2020-12-23 13:25:59 -0800

    changed to integer response for shop

--- assets/js/game.js
+++ assets/js/game.js
@@ -245,21 +245,20 @@ var endGame = function() {
 var shop = function() {
    // Ask the player what they'd like to do
    var shopOptionPrompt = window.prompt(
-      "Would you like to REfILL your health, UPGRADE your attack, or LEAVE the store?  Please enter one: 'REFILL', 'UPGRADE', or 'LEAVE' to make a choice."
+      "Would you like to REfILL your health, UPGRADE your attack, or LEAVE the store?  Please enter 1 for REFILL, 2 for UPGRADE, or 3 to LEAVE."
    );
+
+   shopOptionPrompt = parseInt( shopOptionPrompt );
 
    // Use switch to carry out action
    switch( shopOptionPrompt ) {
-      case "REFILL":
-      case "refill":
+      case 1:
          playerInfo.refillHealth();
          break;
-      case "UPGRADE":
-      case "upgrade":
+      case 2:
          playerInfo.upgradeAttack();
          break;
-      case "LEAVE":
-      case "leave":
+      case 3:
          window.alert( "Leaving the store." );
    
          // Do nothing, the function will end
